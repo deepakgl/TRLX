c28e39636f0643b1a7f68dc887dd56c262707b1e
deepak.kumar <deepak.kumar@srijans-MacBook-Pro-6.local>
2019-09-24 14:55:20 +0530

    ELX-262: Added validation in level listing api.

--- elx-d8/docroot/modules/custom/trlx_utility/src/Utility/CommonUtility.php
+++ elx-d8/docroot/modules/custom/trlx_utility/src/Utility/CommonUtility.php
@@ -371,4 +371,60 @@ class CommonUtility {
     return [];
   }
 
+  /**
+   * Get term nodes by tid.
+   *
+   * @param int $tids
+   *   Term id.
+   * @param Object $_userData
+   *   User information.
+   * @param int $tids
+   *   Term id.
+   *
+   * @return string
+   *   Return term name.
+   */
+  public function getTermNodes($tids, $_userData, $language) {
+    $regions = $_userData->region;
+    $subregions = $_userData->subregion;
+    $country = $_userData->country;
+    // Get region, subregion or country from token if array is not empty.
+    if (!empty($country)) {
+      $ref_keys = $country;
+    }
+    elseif (!empty($subregions)) {
+      $ref_keys = $subregions;
+    }
+    elseif (!empty($regions)) {
+      $ref_keys = $regions;
+    }
+    $user_utility = new UserUtility();
+    // Get current user markets.
+    $markets = array_column($user_utility->getMarketByReferenceId($ref_keys), 'entity_id');
+    $query = \Drupal::database();
+    $query = $query->select('node_field_data', 'n');
+    $query->join('node__field_learning_category', 'nflc', 'n.nid = nflc.entity_id');
+    $query->join('node__field_point_value', 'nfpv', 'n.nid = nfpv.entity_id');
+    $query->distinct('nid');
+    $query->fields('n', ['nid']);
+    $query->fields('nflc', ['field_learning_category_target_id']);
+    $query->fields('nfpv', ['field_point_value_value']);
+    $query->condition('nflc.field_learning_category_target_id', $tids, 'IN');
+    $query->condition('n.status', '1');
+    $query->condition('n.langcode', $language);
+    $query->condition('n.type', 'level_interactive_content');
+    $query->condition('nflc.langcode', $language);
+    $query->join('node__field_markets', 'nfm', 'n.nid = nfm.entity_id');
+    $query->condition('nfm.field_markets_target_id', $markets, 'IN');
+    $result = $query->execute()->fetchAll();
+    foreach ($result as $key => $value) {
+      $arr[$value->field_learning_category_target_id][] = [
+        'nid' => $value->nid,
+        'point_value' => $value->field_point_value_value,
+      ];
+    }
+
+    return $arr;
+  }
+
 }
