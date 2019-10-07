a0f657ed410cc2b5b66ec2a2593a0c1eb5044938
sandesh.jain <sandesh.jain@srijan.net>
2019-10-07 16:02:14 +0530

    TRLX-576: Image help text added

--- docroot/modules/custom/trlx_lang_translation/src/Utility/LangUtility.php
+++ docroot/modules/custom/trlx_lang_translation/src/Utility/LangUtility.php
@@ -53,4 +53,73 @@ class LangUtility {
     return $results;
   }
 
+  /**
+   * Fetch primary and secondary language by market tid.
+   *
+   * @param int $market_id
+   *   Market tid.
+   * @param string $flag
+   *   Flag name.
+   *
+   * @return array
+   *   Market primary and secondary language.
+   */
+  public function getMarketPrimaryAndSecondaryLanguage($market_id, $flag = NULL) {
+    $query = \Drupal::database();
+    $query = $query->select('taxonomy_term__field_primary_language', 'pl');
+    $query->distinct();
+    $query->leftjoin('taxonomy_term__field_secondary_language',
+    'sl', 'pl.entity_id = sl.entity_id');
+    $query->fields('pl', ['field_primary_language_target_id']);
+    $query->fields('sl', ['field_secondary_language_target_id']);
+    $query->condition('pl.entity_id', $market_id, 'IN');
+    $results = $query->execute()->fetchAll();
+    if (!empty($flag)) {
+      return $results;
+    }
+    $primary_lang = array_column(
+     $results, 'field_primary_language_target_id');
+    $secondary_lang = array_column(
+     $results, 'field_secondary_language_target_id');
+    $lang_code = array_unique(array_merge(
+     $primary_lang, $secondary_lang));
+    $response = [];
+    foreach ($lang_code as $key => $value) {
+      if (!empty($value)) {
+        $language = \Drupal::languageManager()->getLanguage($value);
+        $response[$value] = $language->getName();
+      }
+    }
+
+    return $response;
+  }
+
+  /**
+   * Fetch Market languages by user id.
+   *
+   * @return array
+   *   User primary and secondary language.
+   */
+  public function getMarketLanguageByUserId() {
+    $user_utility = new UserUtility();
+    $roles = $user_utility->getUserRoles(\Drupal::currentUser()->id());
+    // Return all languages for global admin role.
+    if (!$roles) {
+      $language = \Drupal::languageManager()->getLanguages();
+      foreach ($language as $key => $value) {
+        $lang[$value->getId()] = $value->getName();
+      }
+    }
+    else {
+      // Fetch market by user id.
+      $market = $user_utility->getMarketByUserId(\Drupal::currentUser()->id(),
+       'all');
+      // Fetch primary and secondary languages by market id.
+      $lang = $this->getMarketPrimaryAndSecondaryLanguage(array_column(
+        $market, 'field_default_market_target_id'));
+    }
+
+    return $lang;
+  }
+
 }
