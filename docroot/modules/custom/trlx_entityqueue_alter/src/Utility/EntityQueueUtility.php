8f2e0c48e9109b23bc2502e9d6485ef38d090561
Srijan-Macbook-Pro <srijan-macbook-pro@srijans-MacBook-Pro-5.local>
2019-10-19 16:53:50 +0530

    TRLX-810-Module-Conversion: trlx entityqueue module alter

---
+++ docroot/modules/custom/trlx_entityqueue_alter/src/Utility/EntityQueueUtility.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\trlx_entityqueue_alter\Utility;
+
+/**
+ * Purpose of this class is to build queues object.
+ */
+class EntityQueueUtility {
+
+  /**
+   * Fetch overridden status.
+   *
+   * @param string $queue_name
+   *   Queue name.
+   *
+   * @return bool
+   *   True or False.
+   */
+  public function fetchQueueOverrideFlagStatus($queue_name) {
+    $query = \Drupal::database()
+      ->select('entity_subqueue__field_queue_override_flag', 'of')
+      ->fields('of', ['field_queue_override_flag_value'])
+      ->condition('of.entity_id', $queue_name, '=')
+      ->execute()->fetchAssoc()['field_queue_override_flag_value'];
+    if ($query) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+}
